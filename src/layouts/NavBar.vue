551e663ab377ca53bcaa300dd7e577f3b2a38b5a
cleytonorocha <cleytoodeoliveirarocha@gmail.com>
2025-03-02 13:48:04 -0300

    feat: NavBar

---
+++ src/layouts/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <div class="container-fluid px-5">
+            <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#works">Works</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+.navbar {
+    background-color: white;
+    font-family: 'Hebbo', sans-serif;
+}
+li{
+    margin-left: 20px;
+}
+a {
+    color: black !important;
+    text-decoration: none;
+    font-size: 1.2rem;
+    font-weight: 510
+}
+</style>
