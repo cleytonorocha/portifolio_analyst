1d580077812534297950929e06674ee6e222c8d0
cleytonorocha <cleytoodeoliveirarocha@gmail.com>
2025-03-02 14:56:42 -0300

    feat: ApresentationView

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
-  <v-app>
-    <NavBar />
-    <v-main>
-      <v-container>
-        <router-view />
-      </v-container>
-    </v-main>
-  </v-app>
+  <div>
+    <nav-bar />
+    <section class="container text-center text-md-start">
+      <apresentation-view-vue />
+    </section>
+  </div>
 </template>
 
 <script>
 import NavBar from "@/layouts/NavBar.vue";
-
+import ApresentationViewVue from "./views/ApresentationView.vue";
 export default {
   name: "App",
 
   components: {
     NavBar,
+    ApresentationViewVue,
   },
-
 };
 </script>
+
+<style>
+* {
+  font-family: "Hebbo", sans-serif;
+}
+section {
+  padding: 80px 0;
+}
+</style>
